2d3dfb386f405e0e61e37e8714114b2668b53493
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2018-11-04 20:55:06 +0000

    R lab 1 done (apart from extensions)

--- R maps 1.R
+++ R maps 1.R
@@ -24,6 +24,9 @@ library(geojsonio)
 library(tidyverse)
 library(rJava)
 
+# Set working directory to project files dir
+# setwd("./") didn't work
+
 # read in some data
 LdnData <- read_csv("https://files.datapress.com/london/dataset/ward-profiles-and-atlas/2015-09-24T14:21:24/ward-profiles-excel-version.csv", na = 'n/a')
 LdnData <- edit(LdnData)
@@ -35,8 +38,10 @@ class(EW)
 LdnPolys <- EW[grep("E09",EW@data$lad15cd),]
 qtm(LdnPolys)
 
+dir.exists(file.path(getwd(),"Boundaries"))
+
 # read shape file from local drive
-BoroughMapSF <- read_shape(file.path("./Boundaries/Local_Authority_Districts_December_2011_GB_BGC.shp"), as.sf = TRUE)
+BoroughMapSF <- read_shape(file.path(getwd(),"Boundaries", "Local_Authority_Districts_December_2011_GB_BGC.shp"), as.sf = TRUE)
 BoroughMapSF <- BoroughMapSF[grep("E09",BoroughMapSF$lad11cd),]
 BoroughMapSP <- LdnPolys
 
@@ -132,12 +137,74 @@ ggplot1 <-ggplot() + geom_sf(mapping = aes(geometry = geometry),
 ggplot1
 
 # correct the palette and add a legend - NOT WORKING
-#pal <- scale_fill_continuous(low = "white", high = "orange", name = "Accessibility")
-pal <- scale_color_brewer(palette="Dark2")
+pal <- scale_fill_continuous(low = "white", high = "orange", name = "Accessibility")
+#pal <- scale_color_brewer(palette="Dark2")
 labels <- labs(list(title = 'Average Public Transport Accessibility 2014', x = 'Longitude', y = 'Latitude'))
 ggplot2 <-  ggplot() + 
             geom_sf(mapping = aes(geometry = geometry),
             fill = BoroughDataMap$Average.Public.Transport.Accessibility.score...2014, data = BoroughDataMap) + 
             pal + 
             labels
+pal
+labels
 ggplot2
+
+# Check coordinate reference system
+print(BoroughMapSP)
+print(BoroughMapSF)
+
+# assigning CRS when reading the data
+BoroughMapSP <- read_shape(file.path("./Boundaries","Local_Authority_Districts_December_2011_GB_BGC.shp"), 
+                          #current.projection = 27700, # not needed since shape file comes with projection
+                          as.sf = FALSE
+                          )
+# This changes it to a SF object - is a wrapper for st_transform which works on SF
+BoroughMapSP <- set_projection(BoroughMapSP, get_proj4(27700))
+print(BoroughMapSP)
+
+# Again, setting the CRS when reading data doesn't reproject
+BoroughMapSF <- st_read(file.path("./Boundaries","Local_Authority_Districts_December_2011_GB_BGC.shp")) %>% st_set_crs(27700)
+print(BoroughMapSF)
+
+# Can also assign CRS once shape reading - again this doesn't reproject
+proj4string(BoroughMapSP) <- CRS("+init=epsg:27700")
+
+# To change CRS need to re-project data
+CRS("+init=epsg:4326")
+CRS("+proj=longlat +datum=WGS84")
+BoroughMapSPWGS84 <- spTransform(BoroughMapSP, CRS("+init=epsg:4326"))
+print(BoroughMapSPWGS84)
+
+# Transform back
+BoroughMapSPBNG <- spTransform(BoroughMapSP, CRS("+init=epsg:27700"))
+print(BoroughMapSP)
+print(BoroughMapSPBNG)
+
+
+# Now that maps can be re-projected allows us to work with various basemaps
+#install.packages('ggmap')
+library(ggmap)
+
+# use the WGS84 projection, joined wit London data
+BoroughDataMapWGS84 <- append_data(BoroughMapSPWGS84, LdnData, key.shp = 'lad11cd', key.data = 'New code', ignore.duplicates = TRUE)
+
+bdm_names <-  names(BoroughDataMapWGS84@data) 
+bdm_names_clean <- gsub("[^[:alnum:][:blank:]?&/\\-]", "", bdm_names)
+names(BoroughDataMapWGS84@data) <-make.names(bdm_names_clean, unique = TRUE)
+
+
+ldnbox1 = c(left = -0.5103766, bottom = 51.28676, right = 0.3340146, top = 51.69187)
+ldnbox2 <- as.vector((st_bbox(BoroughMapSPWGS84)))
+
+map <- get_stamenmap(ldnbox2, zoom= 10, maptype = 'toner-lite')
+colnames(BoroughDataMapWGS84@data)
+BDMWGS84 <- st_as_sf(BoroughDataMapWGS84)
+
+# cRSs dont match up
+ggmap(map) +geom_sf(mapping = aes(geometry = geometry), 
+                    fill = BDMWGS84$Crime.rate...2014.15, 
+                     data = BDMWGS84,
+                     inherit.aes = FALSE,
+                     alpha = 0.7) + theme_minimal()
+
+
