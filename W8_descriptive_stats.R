3b21371aed37e9c932248935a4824b9236a813db
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2018-12-20 14:40:11 +0000

    w8 first commit

---
+++ W8_descriptive_stats.R
@@ -0,0 +1,15 @@
+####################
+#
+# Week 8 Practical
+#
+####################
+
+library(tidyverse)
+library(downloader)
+library(rgdal)
+library(sf)
+library(ggplot2)
+library(reshape2)
+library(plotly)
+library(highcharter)
+
