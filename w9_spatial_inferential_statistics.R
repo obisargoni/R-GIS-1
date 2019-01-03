25c92d841c871a223e4e52d1a51e4019d684cf59
Obi Thompson Sargoni <othompsarg@hotmail.com>
2019-01-03 17:56:19 +0000

    W9 Reading in and checking data

--- w9_spatial_inferential_statistics.R
+++ w9_spatial_inferential_statistics.R
@@ -1,1 +1,17 @@
 # Week 9 practical - Spatial Inferential Statistics
+
+library(tidyverse)
+library(downloader)
+library(rgdal)
+library(sf)
+library(ggplot2)
+library(reshape2)
+library(plotly)
+library(highcharter)
+
+rm(LondonWards)
+
+LondonWards <- readOGR(file.path(getwd(), 'Data'), layer = 'NewLondonWardsUpdated.shp')
+
+# Check SP object has recoded variables
+LondonWards[, grepl('GCSE', colnames(LondonWards@data))]
