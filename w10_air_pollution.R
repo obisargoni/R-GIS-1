0a989acc8461671ff82d612e459028fc497ea3c2
Obi Thompson Sargoni <othompsarg@hotmail.com>
2019-01-10 16:14:35 +0000

    w10 Completed Kriging interpolation, but resulting plot doesn't look good. Need to retry

--- w10_air_pollution.R
+++ w10_air_pollution.R
@@ -11,7 +11,7 @@ AirPolData <- read.csv(file.path(getwd(), 'Data', '4.1. Concentrations LAEI 2013
 length(AirPolData)
 
 # Creat a smaller dataset so that interpolation runs more quickly
-AirPolData <- AirPolData[sample(nrow(AirPolData), 500000),]
+AirPolData <- AirPolData[sample(nrow(AirPolData), 5000),]
 
 # Check the fields
 cbind(colnames(AirPolData),lapply(colnames(AirPolData), class))
@@ -72,3 +72,62 @@ plt_AirPol
 # Interpolation by Kriging
 #
 ####################
+
+# First need to construct a semivariogram model in order to describe the spatial autocorrelation of the data
+
+?zerodist
+
+# Remove Air Quality measurements that have the same coordinates
+class(zerodist(AirPolData))
+
+# Create empirical semivariogram 
+variogmcloud <- variogram(conct ~ 1, locations = AirPolData, data = AirPolData, cloud = TRUE)
+plot(variogmcloud)
+
+# Create a semi variogram by binning values in the variogram and plotting the 'lags' of each bin - half the mean value (why this?)
+semivariogm <- variogram(conct ~ 1, locations = AirPolData, data = AirPolData)
+plot(semivariogm)
+semivariogm
+
+# From this can get the sill, range and nugget; measures of the data's spatial autocorrelation
+class(semivariogm)
+
+# Fit a polynomial to the data
+?poly
+model_sv <- lm(semivariogm$gamma ~ poly(semivariogm$dist, 3))
+summary(model_sv) # The model coefficient for the intercept does not agree with the semivariogram plot
+
+sv_nugget <-  model_sv$coefficients['(Intercept)']
+
+# Tried getting the derivative of the model equation in order to find the sill argument but doesn't seem
+# to be a quick way to fin the x values for which the model equation gradient (ie the derivative) isclose to zero but this is complicated
+model_expr <- expression(79.4 + 86*x -14*x^2 + -10*x^3)
+deriv_model <- deriv(model_expr, 'x')
+class(deriv_model)
+
+sv_sill <-  100
+sv_range <- 20000
+sv_nugget <- 30
+
+vgm()
+# Now build a model semivariogram using the sill range and nugget
+model.variogm <- vgm(psil = sv_sill, model = 'Gau', nugget = sv_nugget, range = sv_range)
+
+# Fit this model to a sample variogram to check the fit
+fit.vgm <-fit.variogram(semivariogm, model.variogm)
+plot(semivariogm, fit.vgm) # Looks OK
+
+# Could also try some alternatives models instead of 'Gau', eg 'Exp'. Tried a few, Gau was best
+
+# Now that a model semivariogram has been obtained, the charateristic spatial autocorrelation of the data is known. This
+# permits Kriging interpolation
+AirPolKrig <- krige(formula = conct ~ 1, locations = AirPolData, newdata = Grd, model = model.variogm)
+krig.output <- as.data.frame(AirPolKrig)
+
+head(krig.output)
+names(krig.output)[3] <- 'conct_krig'
+head(krig.output)
+
+# Now plot the interpolated data
+AirPolKrig_plot <- ggplot(data = krig.output, aes(x=x, y=y)) + geom_tile(aes(fill = conct_krig)) + scale_fill_gradient(low = '#FEEBE2', high = '#7A0177')
+AirPolKrig_plot # Plot does not look ok! Must have made a mistake somewhere.
